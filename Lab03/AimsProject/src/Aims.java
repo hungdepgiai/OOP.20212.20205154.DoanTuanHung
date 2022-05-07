0f9e0de485f428f79f8f976cc4eb2dbaa4331a65
hungdepgiai <103294332+hungdepgiai@users.noreply.github.com>
2022-05-07 16:48:10 +0700

    Add files via upload

---
+++ Lab03/AimsProject/src/Aims.java
@@ -0,0 +1,16 @@
+
+public class Aims {
+	public static void main(String[]args)
+	{
+cart anOrder = new cart();
+DigitalVideoDisc dvd1= new DigitalVideoDisc("The Lion King","Animation","Roger Allers",87,19.95f);
+anOrder.addDigitalVideoDisc(dvd1);
+DigitalVideoDisc dvd2= new DigitalVideoDisc("Star Wars","Science Fiction","George Lucas",87,24.95f);
+anOrder.addDigitalVideoDisc(dvd2);
+DigitalVideoDisc dvd3= new DigitalVideoDisc("Aladin","Animation",18.99f);
+anOrder.addDigitalVideoDisc(dvd3);
+anOrder.removeDigitalVideoDisc(dvd3);
+System.out.println("The total cost is:");
+System.out.println(anOrder.totalCost());
+	}
+}
